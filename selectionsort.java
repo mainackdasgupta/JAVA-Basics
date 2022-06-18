5090235c4beee9678f0ba221d453337fbaf7cc3c
Mai123 <dasguptamainack@gmail.com>
2022-06-18 19:57:42 +0530

    updated

--- selectionsort.java
+++ selectionsort.java
@@ -1,14 +1,24 @@
+import java.util.* ;
 public class selectionsort {
 
-    public static void printArray(int arr[]) {
+     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
-    }
+    } 
 
     public static void main(String args[]) {
-        int arr[] = { 7, 8, 1, 3, 2 };
+        /*int arr[] = { 7, 8, 1, 3, 2 };    */      
+        
+        // taking the array as user input.
+         Scanner Sc = new Scanner(System.in);
+         int Size = Sc.nextInt();
+         int arr[] = new int[Size];
+
+         for(int i = 0; i<Size; i++){
+            arr[i] = Sc.nextInt();
+         }
 
         // selection sort
         for (int i = 0; i < arr.length - 1; i++) {
